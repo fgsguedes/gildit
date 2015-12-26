a69b866171527028ef065f1635e052ccb0890729
Filipe Guedes <fgs.guedes@gmail.com>
2015-12-26 17:37:24 -0200

    Removed unused method

--- app/src/main/kotlin/br/com/hardcoded/gildit/App.kt
+++ app/src/main/kotlin/br/com/hardcoded/gildit/App.kt
@@ -1,24 +1,16 @@
 package br.com.hardcoded.gildit
 
 import android.app.Application
-import android.util.Log
 import br.com.hardcoded.gildit.injecting.AndroidModule
 import br.com.hardcoded.gildit.injecting.module.ApplicationComponent
 import br.com.hardcoded.gildit.injecting.module.DaggerApplicationComponent
 
 class App : Application() {
 
-  val TAG = App::class.simpleName
-
   val component: ApplicationComponent by lazy {
     DaggerApplicationComponent
         .builder()
         .androidModule(AndroidModule(this))
         .build()
-  }
-
-  override fun onCreate() {
-    super.onCreate()
-    Log.e(TAG, "onCreate")
   }
 }
