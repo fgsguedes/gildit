77b69157f335cc717de441055af7251b4e386255
Filipe Guedes <fgs.guedes@gmail.com>
2015-12-29 17:55:50 -0200

    Json object as private

--- app/src/main/kotlin/br/com/hardcoded/gildit/extension/Json.kt
+++ app/src/main/kotlin/br/com/hardcoded/gildit/extension/Json.kt
@@ -3,7 +3,7 @@ package br.com.hardcoded.gildit.extension
 import com.google.gson.Gson
 import com.google.gson.JsonElement
 
-object Json {
+private object Json {
   private val gson = Gson()
 
   fun <T> fromJson(jsonElement: JsonElement, targetClass: Class<T>) = gson.fromJson(jsonElement, targetClass)
