50cbd226da587df7d68e3f631658cb03b6dc8af8
Filipe Guedes <fgs.guedes@gmail.com>
2016-01-11 14:44:39 -0200

    Using lazy findViewById over Kotlin Android Extensions

--- app/src/main/kotlin/br/com/hardcoded/gildit/ui/activity/LinkListActivity.kt
+++ app/src/main/kotlin/br/com/hardcoded/gildit/ui/activity/LinkListActivity.kt
@@ -2,17 +2,21 @@ package br.com.hardcoded.gildit.ui.activity
 
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
 import br.com.hardcoded.gildit.R
 import br.com.hardcoded.gildit.model.Thing
 import br.com.hardcoded.gildit.presenter.LinksListPresenter
 import br.com.hardcoded.gildit.view.LinksListView
-import kotlinx.android.synthetic.main.activity_submission_list.*
 import javax.inject.Inject
 
 class LinkListActivity : BaseActivity(), LinksListView {
 
   @Inject
   lateinit var presenter: LinksListPresenter
+
+  val linksRecycleView by lazy { findViewById(R.id.linksRecycleView) as RecyclerView }
+  val toolBar by lazy { findViewById(R.id.toolBar) as Toolbar }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
